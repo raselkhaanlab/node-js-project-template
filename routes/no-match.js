6bbc7fe545628550f2e7cf4456bde4fe9cb49fda
ANDROIDFARYA\ASUS <raselkhanlab@gmail.com>
2020-01-02 16:36:18 +0600

    404.ejs add

--- routes/no-match.js
+++ routes/no-match.js
@@ -2,6 +2,9 @@ function noMatch(req,res,next){
 	if(req.xhr){
 		return res.send({message:'route not match'});
 	}
-	// return res.render('404',{message:'404 not found'});
+	else{
+		return res.render('404',{message:'404 not found'});
+	}
+	
 }
 module.exports = noMatch;
